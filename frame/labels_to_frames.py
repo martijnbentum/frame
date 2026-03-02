0faa5d61dc3323bed44b57b1060bc265a3a5bb93
martijnbentum <bentummartijn@gmail.com>
2026-03-02 18:00:47 +0100

    added middle_point and center_frame_on_target

--- frame/labels_to_frames.py
+++ frame/labels_to_frames.py
@@ -30,3 +30,12 @@ def labels_to_frames(labels, start= None, end = None, stride = 0.02,
                 frame.labels.append(label.label)
     return f
 
+def middle_point(start, end):
+    return (start + end) / 2
+
+def center_frame_on_target(target, field = 0.025):
+    middle = middle_point(target.start_seconds, target.end_seconds)
+    start_target_frame = middle - (field / 2)
+
+    
+    
